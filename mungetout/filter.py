c55470f9e5483177e452dfce07fd0e71985d8039
Will Szumski <will@stackhpc.com>
2020-02-03 15:15:38 +0000

    Ignore nodes with missing ipmi details

--- mungetout/filter.py
+++ mungetout/filter.py
@@ -1,4 +1,5 @@
 #!/bin/python
+from __future__ import print_function
 import sys
 from ipaddress import ip_address as IP
 import json
@@ -11,6 +12,9 @@ def main():
     start_ip, end_ip = line.split('-')
     encoding = sys.stdout.encoding
     for node in nodes_in:
+        if "ipmi_address" not in node["Driver Info"]:
+            print("Skipping node: %s" % node["UUID"], file=sys.stderr)
+            continue
         ip = IP(node["Driver Info"]["ipmi_address"].decode(encoding))
         if IP(start_ip.decode(encoding)) <= ip <= IP(end_ip.decode(encoding)):
             nodes_out += [node]
